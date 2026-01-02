1d8d2376ba39b2d0745cd295652bb876fa917166
Kavindu Sandeepa Amaranath <kavindusandeepa20031030@gmail.com>
2026-01-03 00:03:44 +0530

    develop authRoutes.js

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const {
+    registerStudent,
+    loginStudent,
+    registerTeacher,
+    loginTeacher,    
+} = require("../controllers/authController")
+
+const router = express.Router();
+router.post("/studentRegister", registerStudent);
+router.post("/studentLogin", loginStudent);
+router.post("/teacherRegister", registerTeacher);
+router.post("/teacherLogin", loginTeacher);
+
+module.exports = router;    
