601b0228c65b649db4f8200da807462751affcb2
Kavindu Sandeepa Amaranath <kavindusandeepa20031030@gmail.com>
2026-01-02 22:33:01 +0530

    develop student.js

---
+++ server/models/student.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const studentschema = new mongoose.Schema({
+    name:{type:String, required:true},
+    stId:{type:String, required:true, unique:true},
+    email:{type:String, required:true, unique:true},
+    password:{type:String, required:true},
+
+},
+    {timestamps: true}
+);
+
+module.exports = mongoose.model.student || mongoose.model("student",studentschema);
